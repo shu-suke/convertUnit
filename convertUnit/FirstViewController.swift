0650619e98708ee318274f691b3a8ec193044306
shu-suke <kinshusuke@gmail.com>
2015-12-16 19:53:40 +0900

    first commit

---
+++ convertUnit/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  convertUnit
+//
+//  Created by kin_shusuke on 2015/12/16.
+//  Copyright © 2015年 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ convertUnit/thirdViewController.swift
@@ -0,0 +1,35 @@
+//
+//  thirdViewController.swift
+//  convertUnit
+//
+//  Created by kin_shusuke on 2015/12/16.
+//  Copyright © 2015年 shusuke. All rights reserved.
+//
+
+import UIKit
+
+class thirdViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
